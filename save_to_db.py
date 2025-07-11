9db3b13b20093ff5c89600eff12dacd1ebe059f4
nvg9478 <nandhini.gutti@gmail.com>
2025-07-11 13:21:39 -0500

    Update save_to_db.py
    everything is referenceable by id now

--- save_to_db.py
+++ save_to_db.py
@@ -1,42 +1,55 @@
-#the textbook said I would need this
 import pickle
 
-#creating the song dictionary/database
+fingerprints_db={}
+with open("fingerprints_db.pkl", mode="wb") as song_file:
+        pickle.dump(fingerprints_db, song_file)
+    
 songs_db={}
 with open("songs_db.pkl", mode="wb") as song_file:
         pickle.dump(songs_db, song_file)
 
-#function takes in the song name, artist, and fingerprint
+
 def save_to_db(name: str, artist: str, fingerprint: tuple):
 
-    #loading the db 
+    #loading the fingerprints db 
+    with open("fingerprints_db.pkl", mode="rb") as opened_file:
+       fp = pickle.load(opened_file)
+
+    #loading the songs db 
     with open("songs_db.pkl", mode="rb") as opened_file:
        songs = pickle.load(opened_file)
+    
+    #generate id #
+    id = len(songs)
     
     #creating song id
     song_id = str(name) + " - " + str(artist)
 
     #checking if song has already been added based on song id
-    for key in songs:
+    for s in songs.values():
         #if the song id already exists, then the function will end and return a message
-        if song_id == key:
+        if song_id == s:
             return "This song has already been added!"
             
     #checking if song has already been added based on fingerprint
-    for f in songs.values():
+    for f in fp.values():
         #if the fingerprint already exists, then the function will end and return a message
         if f == fingerprint:
             return "This song has already been added!"
-            
     
-    #adding song into dict
-    songs[song_id] = fingerprint 
+    #adding fingerprint into dict
+    fp[id] = fingerprint
+
+    #adding song into db
+    songs[id] = song_id
     
-    #saving the dict -> am I supposed to specify where this goes? is it just going to save?
+    #saving song db
     with open("songs_db.pkl", mode="wb") as song_file:
         pickle.dump(songs, song_file)
 
-    #returning the dict
-    return songs
+    #saving fingerprint db
+    with open("fingerprints_db.pkl", mode="wb") as fp_file:
+        pickle.dump(fp, fp_file)
 
-
+    #returning the fingerprints dict
+    return fp
