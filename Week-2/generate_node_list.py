d07a6fd0f9d32f529236ddb49dcefd1c62334bc7
achaparala01 <achaparala01@gmail.com>
2025-07-18 11:46:39 -0400

    Update generate_node_list.py

--- Week-2/generate_node_list.py
+++ Week-2/generate_node_list.py
@@ -1,6 +1,10 @@
 from generate_adjacency_matrix.py import generate_adjacency_matrix
 from create_node_list.py import create_node_list
 from cos_dist.py import cos_dist
+from whispers.py import whispers
+
+def imgpaths_to_graph(paths: list[str], C: float):
+    whispers(generate_node_list(paths, C))
 
 def generate_node_list(paths: list[str], C: float) -> (np.ndarray, list):
     descriptors = img_to_descriptors_array(paths)
