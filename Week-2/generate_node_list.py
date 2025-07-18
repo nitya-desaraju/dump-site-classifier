0eb2bf7d672d565789c0a155b4894a5ac406b9d4
achaparala01 <achaparala01@gmail.com>
2025-07-18 11:23:23 -0400

    Create generate_node_list.py

---
+++ Week-2/generate_node_list.py
@@ -0,0 +1,10 @@
+from generate_adjacency_matrix.py import generate_adjacency_matrix
+from create_node_list.py import create_node_list
+from cos_dist.py import cos_dist
+
+def generate_node_list(paths: list[str], C: float) -> (np.ndarray, list):
+    descriptors = img_to_descriptors_array(paths)
+    dists = cos_dist(descriptors, descriptors)
+    adjs = generate_adjacency_matrix(dists, C)
+    nodes = create_node_list(paths, adjs)
+    return adjs, nodes
