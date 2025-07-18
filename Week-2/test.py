a5d202910d6b04d773f9a7e848cf787cb31d99c2
nitya-desaraju <nitya.desaraju@gmail.com>
2025-07-18 14:34:58 -0500

    Update test.py

--- Week-2/test.py
+++ Week-2/test.py
@@ -8,10 +8,8 @@ if image.shape[-1] == 4:
     # Image is RGBA, where A is alpha -> transparency
     image = image[..., :-1]  # png -> RGB
 
-descriptors, filtered_boxes = img_to_descriptors(image, 0.5)
-
-if descriptors == -1:
-    print("No face detected")
-else:
+try: 
+    descriptors, filtered_boxes = img_to_descriptors(image, 0.91)
     predict_face_from_image(descriptors, filtered_boxes, image, 0.5)
-    
+except:
+    print("No face detected")
