8f6beb24c97328f4c89776606042d530c04540fd
adityapanangat <165591898+adityapanangat@users.noreply.github.com>
2025-07-18 12:48:55 -0500

    Update img_to_descriptors.py

--- Week-2/img_to_descriptors.py
+++ Week-2/img_to_descriptors.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 
-def img_to_descriptors(pic):
+def img_to_descriptors(pic, theshold):
     model = FacenetModel()
     boxes, probabilities, landmarks = model.detect(pic)
 
     #This function checks if an image is below the probability theshold.
-    def filter_false_positive(image, boxes, probs, threshold=0.9):
+    def filter_false_positive(image, boxes, probs, threshold):
         #Converts boxes and probabilities into arrays if needed
         boxes = np.array(boxes)
         probs = np.array(probs)
@@ -19,7 +19,7 @@ def img_to_descriptors(pic):
         filtered_boxes = boxes[keep]
         return image, filtered_boxes
 
-    _, filtered_boxes = filter_false_positive(pic, boxes, probabilities)
+    _, filtered_boxes = filter_false_positive(pic, boxes, probabilities, threshold)
     descriptors = model.compute_descriptors(pic, filtered_boxes)
     
     return descriptors, filtered_boxes
