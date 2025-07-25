64005cca8f8492f78d7cfc0ae1576022cd5bd7bd
Faerocist128 <vasinsr12@gmail.com>
2025-07-25 09:19:37 -0500

    Update test_train_id.py

--- Week-3/test_train_id.py
+++ Week-3/test_train_id.py
@@ -11,22 +11,27 @@ def test_train_id(image_ids_updated_pkl: str):
     train_items = keys[:int(keys.shape[0]*0.8)]
     test_items = keys[int(keys.shape[0]*0.8):]
 
-    train_data = []
+    train_data = [[],[],[]]
     for i, k in enumerate(train_items):
         image_id = k.name
         caption_id = k.caption_ID[random.randint(0, 4)]
         confusor_id = train_items[random.randint(0, train_items.shape[0]-1)].name
-        ar = (image_id, caption_id, confusor_id)
-        train_data.append(ar)
+        train_data[0].append(caption_id)
+        train_data[1].append(image_id)
+        train_data[2].append(confusor_id)
+        #ar = (image_id, caption_id, confusor_id)
+        #train_data.append(ar)
 
     
-    test_data = []
+    test_data = [[],[],[]]
     for i, k in enumerate(test_items):
         image_id = k.name
         caption_id = k.caption_ID[random.randint(0, 4)]
         confusor_id = test_items[random.randint(0, test_items.shape[0]-1)].name
-        ar = (image_id, caption_id, confusor_id)
-        # print(ar)
-        test_data.append(ar)
+        test_data[0].append(caption_id)
+        test_data[1].append(image_id)
+        test_data[2].append(confusor_id)
+        #ar = (image_id, caption_id, confusor_id)
+        #test_data.append(ar)
 
     return (train_data, test_data)
