ff08cde1165ba8e252db0d09dd4282f1ac514731
nitya-desaraju <nitya.desaraju@gmail.com>
2025-07-25 12:42:39 -0500

    Update weigh_query.py

--- Week-3/weigh_query.py
+++ Week-3/weigh_query.py
@@ -60,9 +60,10 @@ def embed_caption(caption_file_path):
             words = caption_text.split() 
 
             for word in words:
-                embedding = W_words[word]
-                idf = idfs[word]
-                caption_vec += embedding * idf #calculates vector
+                if word in W_words:
+                    embedding = W_words[word]
+                    idf = idfs[word]
+                    caption_vec += embedding * idf #calculates vector
             
             magnitude = np.linalg.norm(caption_vec) #
             if magnitude > 0:
