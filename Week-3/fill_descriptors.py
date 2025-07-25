f2976f8151d56e3ec4744fca99e701fbbd1c4610
veeradesale <87218079+veeradesale@users.noreply.github.com>
2025-07-25 07:10:56 -0700

    fill_descriptors.py

--- Week-3/fill_descriptors.py
+++ Week-3/fill_descriptors.py
@@ -1,23 +1,34 @@
 import pickle
+from cogworks_data.language import get_data_path
+from pathlib import Path
 
-def fill_descriptors(features_path, image_ids_path="image_ids.pkl"):
+class image_id:
+    def __init__(self, name, url):
+        self.name = name
+        self.url= url
+        self.caption_ID = []
+        self.descriptor= None
+        self.W= None
+
+def fill_descriptors(image_ids_path="image_ids.pkl"):
     with open(image_ids_path, "rb") as f:
         image_ids = pickle.load(f)
 
-    with open(features_path, "rb") as f:
+    with Path(get_data_path('resnet18_features.pkl')).open('rb') as f:
         resnet18_features = pickle.load(f)
 
     updated_images = []
 
     for image in image_ids:
-        original_name = image.name
-        formatted_name = f"COCO_val2014_{int(original_name):012}.jpg"
+        name = image.name
 
-        if formatted_name in resnet18_features:
-            image.descriptor = resnet18_features[formatted_name]
+        if name in resnet18_features:
+            image.descriptor = resnet18_features[name]
             updated_images.append(image)
         else:
             continue
 
-    with open("image_ids_updated.pkl", "wb") as f:
+    with open("/Users/veeradesale/Desktop/updated_images/image_ids_updated.pkl", "wb") as f:
         pickle.dump(updated_images, f)
+
+fill_descriptors(image_ids_path="image_ids.pkl")
