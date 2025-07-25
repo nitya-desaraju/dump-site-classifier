fef920e193390bde99990b7304ef84a799ff0b66
Faerocist128 <vasinsr12@gmail.com>
2025-07-25 09:41:54 -0500

    Pushing class objects to train & test set instead of ids and names

--- Week-3/test_train_id.py
+++ Week-3/test_train_id.py
@@ -13,21 +13,27 @@ def test_train_id(image_ids_updated_pkl: str):
 
     train_data = [[],[],[]]
     for i, k in enumerate(train_items):
-        image_id = k.name
+        #image_id = k.name
+        #caption_id = k.caption_ID[random.randint(0, 4)]
+        #confusor_id = train_items[random.randint(0, train_items.shape[0]-1)].name
+        image_id = k
         caption_id = k.caption_ID[random.randint(0, 4)]
-        confusor_id = train_items[random.randint(0, train_items.shape[0]-1)].name
+        confusor_id = train_items[random.randint(0, train_items.shape[0]-1)]
         train_data[0].append(caption_id)
         train_data[1].append(image_id)
         train_data[2].append(confusor_id)
         #ar = (image_id, caption_id, confusor_id)
         #train_data.append(ar)
-
+    
     
     test_data = [[],[],[]]
     for i, k in enumerate(test_items):
-        image_id = k.name
+        #image_id = k.name
+        #caption_id = k.caption_ID[random.randint(0, 4)]
+        #confusor_id = test_items[random.randint(0, test_items.shape[0]-1)].name
+        image_id = k
         caption_id = k.caption_ID[random.randint(0, 4)]
-        confusor_id = test_items[random.randint(0, test_items.shape[0]-1)].name
+        confusor_id = train_items[random.randint(0, train_items.shape[0]-1)]
         test_data[0].append(caption_id)
         test_data[1].append(image_id)
         test_data[2].append(confusor_id)
