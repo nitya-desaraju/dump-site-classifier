99b637d96340dfc62fc933369d4237a66001566f
nvg9478 <nandhini.gutti@gmail.com>
2025-07-30 13:19:44 -0500

    Create draw_bounding_boxes.py

---
+++ Capstone/draw_bounding_boxes.py
@@ -0,0 +1,25 @@
+def draw_bounding_boxes(height, width, x, y, image, show_graph=False):
+    x_min = int(x-width/2)
+    x_max = int(x+width/2)
+    y_min = int(y-height/2)
+    y_max = int(y+height/2)
+    print(x_min, x_max, y_min, y_max)
+    cropped_image = image[y_min : y_max, x_min: x_max]
+
+    if show_graph:
+        x_line = np.linspace(x_min, x_max, 100)
+        y_line = np.linspace(y_min, y_max, 100)
+    
+        plt.plot(x_line, np.ones(100) * y_min, color='red')
+        plt.plot(x_line, np.ones(100) * y_max, color='red')
+        plt.plot(np.ones(100) * x_min, y_line, color='red')
+        plt.plot(np.ones(100) * x_max, y_line, color='red')
+    
+        plt.imshow(image)
+    
+        file_name = "figure " + str(random.randint(0, 1000)) + ".jpg"
+        plt.savefig(file_name, dpi=600) 
+
+        plt.show()
+
+    return cropped_image
